e45aa986360ff7522388aa67479998e1f4bd68ab
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-06-17 16:48:57 -0300

    style: format long conditional statements to single lines

--- microshell.c
+++ microshell.c
@@ -8,17 +8,14 @@ int	err(char *str)
 		write(2, str++, 1);
 	return (1);
 }
-
 int	cd(char **argv, int i)
 {
 	if (i != 2)
 		return (err("error: cd: bad arguments\n"));
 	if (chdir(argv[1]) == -1)
-		return (err("error: cd: cannot change directory to "), err(argv[1]),
-			err("\n"));
+		return (err("error: cd: cannot change directory to "), err(argv[1]), err("\n"));
 	return (0);
 }
-
 int	exec(char **argv, int i, char **envp)
 {
 	int	fd[2];
@@ -35,8 +32,7 @@ int	exec(char **argv, int i, char **envp)
 	if (!pid)
 	{
 		argv[i] = 0;
-		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1
-				|| close(fd[1]) == -1))
+		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 			return (err("error: fatal\n"));
 		if (!strcmp(*argv, "cd"))
 			return (cd(argv, i));
@@ -44,12 +40,10 @@ int	exec(char **argv, int i, char **envp)
 		return (err("error: cannot execute "), err(*argv), err("\n"));
 	}
 	waitpid(pid, &status, 0);
-	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1
-			|| close(fd[1]) == -1))
+	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 		return (err("error: fatal\n"));
 	return (WIFEXITED(status) && WEXITSTATUS(status));
 }
-
 int	main(int argc, char **argv, char **envp)
 {
 	int	i;
